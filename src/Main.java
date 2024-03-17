c22093a94df205e1f748188f3a4a15f72901eec2
AlexeyGoryachev <iwmoney11977@gmail.com>
2024-03-17 23:05:01 +0300

    Task 1 is done.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Homework 10.");
+        System.out.println("Task 1.");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String secondName = "Ivanov";
+        String fullName = secondName + ' ' + firstName+ ' ' + middleName;
+        System.out.println(fullName);
     }
 }
